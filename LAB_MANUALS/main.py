04b99ac27a7eeb5b2343f5477f69942c0e96f811
nkkha-dev <khanguyen@appvity.com>
2023-09-12 10:59:12 +0700

    Add labs python

---
+++ LAB_MANUALS/main.py
@@ -0,0 +1,86 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+from simple_ai import *
+from uart import *
+
+AIO_FEED_IDS = ["nutnhan1","nutnhan2"]
+AIO_USERNAME = "nkkha"
+AIO_KEY = "aio_kyaU84hkLqBkQENion4ghfvEQnhX"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDS:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
+    if feed_id == "nutnhan1":
+        if payload == "0":
+            writeData(1)
+        else:
+            writeData(2)
+    elif feed_id == "nutnhan2":
+        if payload == "0":
+            writeData(3)
+        else:
+            writeData(4)
+            
+            
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+counter = 10
+counter_ai = 5
+sensor_type = 0
+previous_result = ""
+
+while True:
+    counter -= 1
+    if counter <= 0:
+        counter = 10
+        #TODO
+        print("Random data is publishing...")
+        if sensor_type == 0:
+            print("Temperature...")
+            temp = random.randint(10, 20)
+            client.publish("cambien1", temp)
+            sensor_type = 1
+        elif sensor_type == 1:
+            print("Humidity...")
+            humi = random.randint(50, 70)
+            client.publish("cambien3", humi)
+            sensor_type = 2
+        elif sensor_type == 2:
+            print("Light...")
+            light = random.randint(100, 500)
+            client.publish("cambien2", light)
+            sensor_type = 0
+
+    counter_ai -= 1
+    if counter_ai <= 0:
+        counter_ai = 5
+        ai_result, image = image_detector()
+        if previous_result != ai_result:
+            previous_result = ai_result
+            print("AI Output: ")
+            client.publish("ai", ai_result)
+            client.publish("image", image)
+
+    readSerial(client)
+
+    time.sleep(1)
+    pass
---
+++ LAB_MANUALS/simple_ai.py
@@ -0,0 +1,64 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
+import numpy as np
+import base64
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = ["0 DEO KHAU TRANG", "1 KHONG DEO KHAU TRANG", "2 KHONG CO NGUOI"]
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+# camera = cv2.VideoCapture("http://172.16.0.205:4747/video")
+camera = cv2.VideoCapture(0)
+
+def image_detector():
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    res, frame = cv2.imencode('.jpg', image)
+    data = base64.b64encode(frame)
+    print(len(data))
+
+    if len(data) > 102400:
+        print("Image is too big!")
+    else:
+        print("Publish image: ")
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    # cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print(" - Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    return class_name[2:], data
+
+# # Listen to the keyboard for presses.
+# keyboard_input = cv2.waitKey(1)
+
+# # 27 is the ASCII for the esc key on your keyboard.
+# if keyboard_input == 27:
+#     break
+
+# camera.release()
+# cv2.destroyAllWindows()
